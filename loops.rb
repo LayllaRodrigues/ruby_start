a277d2159bc6d76e93363fcfe7a0c1ef1bbcb09d
Laylla  de Oliveira Rodrigues <laylla.rodrigues@C0167.local>
2021-08-03 18:40:23 -0300

    loops

---
+++ loops.rb
@@ -0,0 +1,29 @@
+#{times repete quantas vezes for inserido no times}
+
+# 5.times do |i|
+#     puts 'Repetindo a mensagem' + i.to_s + 'vez(es).'
+    
+# end
+
+#{O while ocorre enquanto uma condição ( true/false) for verdadeira}
+
+#{exemplo:}
+# init = 0
+
+# while init <= 10 do 
+#     puts 'Repetindo a mensagem' + init.to_s + 'vez(es).'
+# init += 1
+# end
+
+# for item in (0...10)
+    #     puts 'Repetindo a mensagem' + i.to_s + 'vez(es).'
+    
+    # end
+
+    vingadores = ['ironman','Hulk', 'Spiderman', 'Thor']
+
+    vingadores.each do |v|
+
+        puts v 
+
+    end
